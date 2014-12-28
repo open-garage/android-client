09f64e307e2bebd9520166f186f3fef3d8925b31
Dustin Steiner <dustin.steiner@gmail.com>
2014-12-28 14:30:56 +0100

    alt beacon tests

--- app/src/main/java/net/alopix/morannon/GarageApp.java
+++ app/src/main/java/net/alopix/morannon/GarageApp.java
@@ -10,6 +10,7 @@ package net.alopix.morannon;
 import android.app.Application;
 import android.content.SharedPreferences;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -20,6 +21,13 @@ import net.alopix.morannon.api.v1.request.ToggleRequest;
 import net.alopix.morannon.service.GarageService;
 import net.alopix.morannon.util.FlowBundler;
 import net.alopix.util.SelfSignedCertHelper;
+
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.Region;
+import org.altbeacon.beacon.powersave.BackgroundPowerSaver;
+import org.altbeacon.beacon.startup.BootstrapNotifier;
 
 import java.util.concurrent.TimeUnit;
 
@@ -32,10 +40,13 @@ import retrofit.RestAdapter;
 import retrofit.client.OkClient;
 import retrofit.converter.GsonConverter;
 
+
 /**
  * Created by dustin on 01.12.2014.
  */
-public class GarageApp extends Application {
+public class GarageApp extends Application implements BootstrapNotifier, BeaconConsumer {
+    private static final String TAG = GarageApp.class.getSimpleName();
+
     private static final String API_SERVER_HOST_KEY = "api_server_host_key";
     private static final String API_SERVER_PORT_KEY = "api_server_port_key";
     private static final String API_TOKEN_KEY = "api_token_key";
@@ -57,11 +68,15 @@ public class GarageApp extends Application {
 
     private OpenGarageService mApiService;
 
+    private BeaconManager mBeaconManager;
+    private BackgroundPowerSaver mBeaconPowerSaver;
+
     @Override
     public void onCreate() {
         super.onCreate();
 
         createApiService();
+        initBeaconManager();
     }
 
     public OpenGarageService createApiService(String endpoint) {
@@ -97,6 +112,14 @@ public class GarageApp extends Application {
             }
         });
         return client;
+    }
+
+    private void initBeaconManager() {
+        mBeaconManager = BeaconManager.getInstanceForApplication(this);
+        mBeaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"));
+        mBeaconManager.bind(this);
+
+        mBeaconPowerSaver = new BackgroundPowerSaver(this);
     }
 
     public FlowBundler getFlowBundler() {
@@ -153,5 +176,32 @@ public class GarageApp extends Application {
 
     private SharedPreferences getPreferences() {
         return getSharedPreferences(GarageApp.class.getSimpleName(), MODE_PRIVATE);
+    }
+
+    @Override
+    public void onBeaconServiceConnect() {
+        Log.d(TAG, "onBeaconServiceConnected");
+
+        mBeaconManager.setMonitorNotifier(this);
+        try {
+            mBeaconManager.startMonitoringBeaconsInRegion(new Region("OpenGarageMonitor", null, null, null));
+        } catch (Exception ex) {
+            Log.d(TAG, "Could not start monitoring", ex);
+        }
+    }
+
+    @Override
+    public void didEnterRegion(Region region) {
+        Log.i(TAG, "didEnterRegion: " + region);
+    }
+
+    @Override
+    public void didExitRegion(Region region) {
+        Log.i(TAG, "didExitRegion: " + region);
+    }
+
+    @Override
+    public void didDetermineStateForRegion(int i, Region region) {
+        Log.i(TAG, "didDetermineStateForRegion: " + i + ", " + region);
     }
 }
